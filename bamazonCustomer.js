f24a7c6078defd22de85bfffb87f08f4435bc701
Sakina Shivji <shivjisakina@gmail.com>
2017-06-22 20:55:14 -0400

    :got ids, name, and price from database

---
+++ bamazonCustomer.js
@@ -0,0 +1,50 @@
+//PSEUDO CODE
+
+// Get mysql and inquirer npm
+// Get 'ids', 'names', 'prices of products for sale'
+// Prompt 1: Ask user for ID of product they want to buy
+// Prompt 2: Ask user how many units they want to buy
+// After user places order, check to see if there's enough stock. (if statement)
+// Else, log "insufficient quantity"
+// If there's sufficient stock, place the order.
+// Update MYSQL
+// Show customer purchase total
+
+//functions:
+
+//getcolumns() to get the `ids`, `names`, and `prices`
+
+
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'Bamazon'
+});
+
+connection.connect(function(err, response) {
+    if (err) throw err;
+
+    console.log("Connected to mysql server with the ID " + connection.threadId)
+
+    getcolumns()
+});
+
+function getcolumns() {
+
+    connection.query("SELECT * FROM `customerView`" , function (queryError, response){
+        if (queryError)
+            throw queryError;
+
+        response.forEach(function (row) {
+            console.log("id = ", "'", row.id, "'",
+                "Product Name = ", "'", row.product_name, "'",
+                "Price:", "'", row.price, "'")
+        });
+    })
+
+}
